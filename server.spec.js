0e1daf08245938479a3374b459fafefc4145ecaf
kiyani <bamba.kiyani@gmail.com>
2019-11-15 06:30:11 -0500

    completed

--- server.spec.js
+++ server.spec.js
@@ -1,5 +1,7 @@
 const server = require("./api/server");
 const request = require ("supertest");
+const db = require ("./database/dbConfig");
+const authModel = require ("./auth/users-model");
 
 // Test 1 : Server is running 
 describe('server', () => {
@@ -9,4 +11,32 @@ describe('server', () => {
           .expect(200);
     });
  });
- 
+
+ // Clear tables before running each test
+
+ describe("auth-router", () => {
+  beforeEach(async () => {
+    await db("users").truncate();
+  });
+
+  describe("POST /api/auth/register", () => {
+    it("user should be created with 201", async () => {
+      await request(server)
+        .post("/api/auth/register")
+        .send({ username: "admin2", password: "admin" })
+        .then(res => {
+          expect(res.status).toBe(201);
+        });
+    });
+
+    it("response use JSON", async () => {
+      await request(server)
+        .post("/api/auth/register")
+        .send({ username: "admin", password: "admin" })
+        .then(res => {
+          expect(res.type).toMatch(/json/i);
+        });
+    });
+  });
+});
+
