88f9ae26dde5aa1173771d7d53c5c5ec6bae1aea
kiyani <bamba.kiyani@gmail.com>
2019-11-15 03:38:07 -0500

    Completed register and login endpoint

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,47 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+
+const Users = require('./users-model');
 
 router.post('/register', (req, res) => {
-  // implement registration
-});
+    let user = req.body;
+    const hash = bcrypt.hashSync(user.password, 11); // hash password
+    const newUser = {
+      username: user.username,
+      password: hash
+    };
+
+   Users.add(newUser)
+    .then(savedUser => {
+        console.log(savedUser);
+        res.status(201).json(savedUser);
+    })
+    .catch(error => {
+        console.log(error);
+        res.status(500).json(error);
+    })
+})
 
 router.post('/login', (req, res) => {
-  // implement login
+    let { username, password } = req.body;
+    Users.findBy({ username })
+        .first()
+        .then(user => {
+						// verify password
+            if (user && bcrypt.compareSync(password, user.password)) {
+                req.session.user = user;
+                res.status(200).json({
+                    message: `Welcome ${user.username}!`,
+                });
+            } else {
+                res.status(401).json({
+                    message: 'Invalid login details'
+                })
+            }
+        })
+        .catch(error => {
+            res.status(500).json(error);
+    })
 });
 
 module.exports = router;
